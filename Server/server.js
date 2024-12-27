d1fb7e65bf7fe25a10822602451379fad238a4c9
Prakash Suthar <prakash.r.sutahr123@gmail.com>
2024-12-27 20:28:08 +0530

    user auth and messages part complete

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
+
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT =  5000;
@@ -9,15 +15,21 @@ const PORT =  5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 // app.get("/", (req , res)=>{
 //     //root route http://localhoast:5000
 //     res.send("hello world");
 // })
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/user", userRoutes);
+
+
+
 app.listen(PORT, ()=> {
     connectToMongoDb()
-    console.log('server running on port ${PORT}')
+    console.log("server running on port ${PORT}")
 });
 
