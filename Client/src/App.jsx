e4ce915af81480e2a7d8da12ce54518252df3246
Prakash Suthar <prakash.r.sutahr123@gmail.com>
2024-12-29 13:50:47 +0530

    updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/home";
-import Login from "./pages/login/Login";
+import Login from "./pages/login/login";
 import SignUp from "./pages/signup/signup";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
